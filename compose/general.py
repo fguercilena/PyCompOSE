f590733210ac857cb72fb36d5bf3fa28ae0bdd3f
David Radice <david.radice@psu.edu>
2022-07-27 11:27:29 -0400

    Added calculation of the sound speed to compose/general.py

--- compose/general.py
+++ compose/general.py
@@ -2,6 +2,7 @@
 Utilities to read general purpose (3D) EOS tables
 """
 
+import h5py
 import numpy as np
 import os
 
@@ -66,7 +67,7 @@ class EOS:
 
     That is, the temperature is the fastest running index
     """
-    def __init__(self, path, metadata: EOSMetadata):
+    def __init__(self, path, metadata: EOSMetadata, dtype=np.float64):
         """
         Initialize an EOS object
 
@@ -74,10 +75,11 @@ class EOS:
         """
         self.path = path
         self.md = metadata
+        self.dtype = dtype
 
-        self.nb = np.loadtxt(os.path.join(path, "eos.nb"), skiprows=2)
-        self.t = np.loadtxt(os.path.join(path, "eos.t"), skiprows=2)
-        self.yq = np.loadtxt(os.path.join(path, "eos.yq"), skiprows=2)
+        self.nb = np.loadtxt(os.path.join(path, "eos.nb"), skiprows=2, dtype=dtype)
+        self.t = np.loadtxt(os.path.join(path, "eos.t"), skiprows=2, dtype=dtype)
+        self.yq = np.loadtxt(os.path.join(path, "eos.yq"), skiprows=2, dtype=dtype)
         self.shape = (self.nb.shape[0], self.yq.shape[0], self.t.shape[0])
 
         L = open(os.path.join(path, "eos.thermo"), "r").readline().split()
@@ -94,18 +96,20 @@ class EOS:
         """
         self.thermo = {}
         for name, desc in self.md.thermo.values():
-            self.thermo[name] = np.empty(self.shape)
+            self.thermo[name] = np.empty(self.shape, dtype=self.dtype)
         with open(os.path.join(self.path, "eos.thermo"), "r") as tfile:
             _ = tfile.readline()
             for line in tfile:
                 L = line.split()
                 it, inb, iyq = int(L[0])-1, int(L[1])-1, int(L[2])-1
                 for iv in range(1, 8):
-                    self.thermo[self.md.thermo[iv][0]][inb, iyq, it] = float(L[2 + iv])
+                    self.thermo[self.md.thermo[iv][0]][inb, iyq, it] = \
+                        float(L[2 + iv])
                 Nadd = int(L[10])
                 for iv in range(8, 8+Nadd):
                     if iv in self.md.thermo:
-                        self.thermo[self.md.thermo[iv][0]][inb, iyq, it] = float(L[2 + 1 + iv])
+                        self.thermo[self.md.thermo[iv][0]][inb, iyq, it] = \
+                            float(L[2 + 1 + iv])
 
     def __read_compo_entries(self):
         """
@@ -113,11 +117,11 @@ class EOS:
         """
         self.Y, self.A, self.Z = {}, {}, {}
         for name, desc in self.md.pairs.values():
-            self.Y[name] = np.zeros(self.shape)
+            self.Y[name] = np.zeros(self.shape, dtype=self.dtype)
         for name, desc in self.md.quads.values():
-            self.Y[name] = np.zeros(self.shape)
-            self.A[name] = np.nan*np.ones(self.shape)
-            self.Z[name] = np.nan*np.ones(self.shape)
+            self.Y[name] = np.zeros(self.shape, dtype=self.dtype)
+            self.A[name] = np.nan*np.ones(self.shape, dtype=self.dtype)
+            self.Z[name] = np.nan*np.ones(self.shape, dtype=self.dtype)
         with open(os.path.join(self.path, "eos.compo"), "r") as cfile:
             for line in cfile:
                 L = line.split()
@@ -133,7 +137,8 @@ class EOS:
                 Nquad = int(L[ix])
                 ix += 1
                 for iq in range(Nquad):
-                    I, A, Z, Y = int(L[ix]), float(L[ix+1]), float(L[ix+2]), float(L[ix+3])
+                    I, A, Z, Y = int(L[ix]), float(L[ix+1]), \
+                                 float(L[ix+2]), float(L[ix+3])
                     ix += 4
                     if I in self.md.quads:
                         name = self.md.quads[I][0]
@@ -141,22 +146,84 @@ class EOS:
                         self.Z[name][inb, iyq, it] = Z
                         self.Y[name][inb, iyq, it] = Y
 
+    def diff_wrt_nb(self, Q):
+        dQdn = np.empty_like(Q)
+        dQdn[1:-1,...] = (Q[2:,...] - Q[:-2,...])/(
+            self.nb[2:,np.newaxis,np.newaxis] - self.nb[:-2,np.newaxis,np.newaxis])
+        dQdn[0,...] = (Q[1,...] - Q[0,...])/(self.nb[1] - self.nb[0])
+        dQdn[-1,...] = (Q[-1,...] - Q[-2,...])/(self.nb[-1] - self.nb[-2])
+        return dQdn
 
-def main():
-    md = EOSMetadata(
-        pairs = {
-            0: ("e", "electron"),
-            10: ("n", "neutro"),
-            11: ("p", "proton"),
-            4002: ("He4", "alpha particle"),
-            3002: ("He3", "helium 3"),
-            3001: ("H3", "tritium"),
-            2001: ("H2", "deuteron")},
-        quads = {
-            999: ("N", "average nucleous")
-        }
-    )
-    eos = EOS("SFHo", md)
+    def diff_wrt_t(self, Q):
+        dQdt = np.empty_like(Q)
+        dQdt[...,1:-1] = (Q[...,2:] - Q[...,:-2])/(
+            self.t[np.newaxis,np.newaxis,2:] - self.t[np.newaxis,np.newaxis,:-2])
+        dQdt[...,0] = (Q[...,1] - Q[...,0])/(self.t[1] - self.t[0])
+        dQdt[...,-1] = (Q[...,-1] - Q[...,-2])/(self.t[-1] - self.t[-2])
+        return dQdt
 
-if __name__ == '__main__':
-    main()
+    def compute_cs2(self):
+        """
+        Computes the square of the sound speed
+        """
+        P = self.thermo["Q1"]*self.nb[:,np.newaxis,np.newaxis]
+        S = self.thermo["Q2"]
+        u = self.mn*(self.thermo["Q7"] + 1)
+        h = u + self.thermo["Q1"] 
+
+        dPdn = self.diff_wrt_nb(P)
+        dSdn = self.diff_wrt_nb(S)
+        dPdt = self.diff_wrt_t(P)
+        dSdt = self.diff_wrt_t(S)
+
+        self.thermo["cs2"] = (dPdn - dSdn/dSdt*dPdt)/h
+        self.md.thermo[12] = ("cs2", "sound speed squared [c^2]")
+
+    def write(self, fname, dtype=np.float64):
+        """
+        Writes the table as an HDF5 file
+        """
+        dfile = h5py.File(fname, "w")
+        dfile.create_dataset("nb", dtype=dtype, data=self.nb,
+            compression="gzip", compression_opts=9)
+        dfile.create_dataset("t", dtype=dtype, data=self.t,
+            compression="gzip", compression_opts=9)
+        dfile.create_dataset("yq", dtype=dtype, data=self.yq,
+            compression="gzip", compression_opts=9)
+        dfile["nb"].attrs["desc"] = "baryon number density [fm^-3]"
+        dfile["t"].attrs["desc"] = "temperature [MeV]"
+        dfile["yq"].attrs["desc"] = "charge fraction"
+
+        dfile.create_dataset("mn", dtype=dtype, data=self.mn)
+        dfile.create_dataset("mp", dtype=dtype, data=self.mp)
+        dfile["mn"].attrs["desc"] = "neutron mass [MeV]"
+        dfile["mp"].attrs["desc"] = "proton mass [MeV]"
+
+        for name, desc in self.md.thermo.values():
+            dfile.create_dataset(name, dtype=dtype, data=self.thermo[name],
+                compression="gzip", compression_opts=9)
+            dfile[name].attrs["desc"] = desc
+
+        for name, desc in self.md.pairs.values():
+            key = "Y[{}]".format(name)
+            dfile.create_dataset(key, dtype=dtype, data=self.Y[name],
+                compression="gzip", compression_opts=9)
+            dfile[key].attrs["desc"] = desc
+
+        for name, desc in self.md.quads.values():
+            key = "Y[{}]".format(name)
+            dfile.create_dataset(key, dtype=dtype, data=self.Y[name],
+                compression="gzip", compression_opts=9)
+            dfile[key].attrs["desc"] = desc
+
+            key = "A[{}]".format(name)
+            dfile.create_dataset(key, dtype=dtype, data=self.A[name],
+                compression="gzip", compression_opts=9)
+            dfile[key].attrs["desc"] = desc
+
+            key = "Z[{}]".format(name)
+            dfile.create_dataset(key, dtype=dtype, data=self.Z[name],
+                compression="gzip", compression_opts=9)
+            dfile[key].attrs["desc"] = desc
+
+        dfile.close()
--- examples/general.py
+++ examples/general.py
@@ -1,6 +1,7 @@
 # Tested using: https://compose.obspm.fr/eos/34
 
 # %%
+import h5py
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import numpy as np
@@ -18,35 +19,101 @@ md = EOSMetadata(
         4002: ("He4", "alpha particle"),
         3002: ("He3", "helium 3"),
         3001: ("H3", "tritium"),
-        2001: ("H2", "deuteron")},
+        2001: ("H2", "deuteron")
+    },
     quads = {
         999: ("N", "average nucleous")
     }
 )
 eos = EOS("SFHo", md)
+eos.compute_cs2()
 
 # %%
-iyq = np.argmin(np.abs(eos.yq - 0.5))
-print("Error on Y_e: ", np.max(np.abs(eos.Y["e"][:,iyq,:] - eos.yq[iyq])))
+eos.write("SFHo/compose.h5")
 
 # %%
+print("{} <= cs2 <= {}".format(eos.thermo["cs2"].min(), eos.thermo["cs2"].max()))
+
+# %% Load comparison table
+c = 29979245800.0           # CGS
+MeV = 1.60217733e-06        # CGS
+fm = 1e-13                  # CGS
+
+ref_table = h5py.File("SFHo/SFHo_hydro_29-Jun-2015.h5", "r")
+ref_t = np.array(ref_table["temperature"])
+ref_ye = np.array(ref_table["ye"])
+ref_mb = np.array(ref_table["mass_factor"])
+ref_rho = np.array(ref_table["density"])
+ref_cs2 = np.array(ref_table["cs2"])
+ref_mb = ref_mb*MeV/c**2
+ref_nb = ref_rho/(ref_mb)*fm**3
+
+# %% Plot CompOSE EOS in pressure dominated regime
+Y_e = 0.5
+iyq = np.argmin(np.abs(eos.yq - Y_e))
+
 plt.figure()
 plt.plot(eos.t, eos.thermo["Q1"][0,iyq,:]/eos.t**4)
 plt.xlabel(r"$T\ [{\rm MeV}]$")
-plt.ylabel(r"$p/(nb*T^4)\ [{\rm MeV^3}]$")
+plt.ylabel(r"$p/(nb*T^4)\ [{\rm MeV}^3]$")
+plt.xscale("log")
+plt.yscale("log")
+
+# %% Compare CompOSE and stallarcollapse.org cs2 at zero density
+Y_e = 0.5
+iyq = np.argmin(np.abs(eos.yq - Y_e))
+iye = np.argmin(np.abs(ref_ye - Y_e))
+
+plt.figure()
+plt.plot(eos.t, eos.thermo["cs2"][0,iyq,:], label="CompOSE")
+plt.plot(ref_t[:], ref_cs2[iye,:,0], label="stellarcollapse.org")
+plt.xlabel(r"$T\ [{\rm MeV}]$")
+plt.ylabel(r"$c_s^2/c^2$")
+plt.axhline(1/3, color='k', label=r"$1/3$")
+plt.legend()
+plt.xscale("log")
+#plt.yscale("log")
+
+# %% Compare CompOSE and stallarcollapse.org cs2 at zero temperature
+Y_e = 0.5
+iyq = np.argmin(np.abs(eos.yq - Y_e))
+iye = np.argmin(np.abs(ref_ye - Y_e))
+
+plt.figure()
+plt.plot(eos.nb, eos.thermo["cs2"][:,iyq,0], label="CompOSE")
+plt.plot(ref_nb, ref_cs2[iye,0,:], label="stellarcollapse.org")
+plt.xlabel(r"$nb\ [{\rm fm}^{-3}]$")
+plt.ylabel(r"$c_s^2/c^2$")
+plt.axhline(1, color='k', label=r"$1$")
+plt.legend()
+plt.xscale("log")
+plt.yscale("log")
+
+
+# %% Plot sound speed for fixed Y_e
+Y_e = 0.2
+iyq = np.argmin(np.abs(eos.yq - Y_e))
+iye = np.argmin(np.abs(ref_ye - Y_e))
+
+plt.figure()
+plt.title("CompOSE")
+plt.pcolormesh(eos.nb, eos.t, eos.thermo["cs2"][:,iyq,:].T,vmin=0, vmax=1, cmap='jet')
+plt.colorbar(label=r"$c_s^2$")
+plt.xlabel(r"$n_b\ [{\rm fm}^{-3}]$")
+plt.ylabel(r"$T\ [{\rm MeV}]$")
+plt.xscale("log")
+plt.yscale("log")
+
+plt.figure()
+plt.title("stellarcollapse.org")
+plt.pcolormesh(ref_nb, ref_t, ref_cs2[iye,:,:],vmin=0, vmax=1, cmap='jet')
+plt.colorbar(label=r"$c_s^2$")
+plt.xlabel(r"$n_b\ [{\rm fm}^{-3}]$")
+plt.ylabel(r"$T\ [{\rm MeV}]$")
 plt.xscale("log")
 plt.yscale("log")
 
 # %%
-plt.figure()
-plt.pcolormesh(eos.nb, eos.t, eos.Y["N"][:,iyq,:].T, norm=LogNorm(1e-15, 1.0))
-plt.colorbar(label=r"$Y_{\rm heavy}$")
-plt.xlabel(r"$n_b\ [{\rm fm}^{-3}]$")
-plt.ylabel(r"$T\ [{\rm MeV}]$")
-#plt.xscale("log")
-plt.yscale("log")
-plt.xlim(1e-11, 0.1)
-plt.ylim(0.1, 100)
+plt.show()
 
 # %%
-plt.show()
